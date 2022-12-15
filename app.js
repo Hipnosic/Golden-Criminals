954e53d3b11880852cbef728a305d75b533e84fb
ImaDuhf <timperson234@gmail.com>
2022-12-15 14:28:27 +0100

    Added product info

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const addProductBtn = document.querySelectorAll('.add-product');
+const products = document.querySelectorAll('.product');
 
 for (const button of addProductBtn) {
   button.addEventListener('click', () => {
     alert('"Product name" added to cart');
+  });
+}
+
+for (const prduct of products) {
+  let clicked = false;
+  prduct.addEventListener('click', (e) => {
+    let targetParent = e.target.parentNode;
+    let span = targetParent.querySelector('.product-info');
+    let image = targetParent.querySelector('.product-img');
+    if (e.target.nodeName != 'BUTTON') {
+      if (clicked == false) {
+        clicked = true;
+        span.setAttribute('style', 'display: block;');
+        image.setAttribute('style', 'filter: blur(20px);');
+      } else if (clicked == true) {
+        clicked = false;
+        span.setAttribute('style', 'display: none;');
+        image.setAttribute('style', 'filter: blur(0px);');
+      }
+    }
   });
 }
