ef96d560624a17ea42ab31fb902d8f699ec0a007
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2022-12-16 11:23:06 +0100

    sent header and footer into js for all sites

--- app.js
+++ app.js
@@ -26,3 +26,43 @@ for (const prduct of products) {
     }
   });
 }
+
+function createHeader(){
+  let headerContainer = document.querySelector('.header-container'); 
+  headerContainer.innerHTML = `
+  <h1 class="header-text">The Kraken Inn</h1>
+  <a href="#">
+    <button type="button" class="languageBtn">ENG</button>
+  </a>
+  <button class="helpBtn"><img src="/icons/assistance.png" alt="Help" height="30" width="30"></button>
+  <div class="logo"></div>`;
+}
+function createFooter(){
+  let FooterContainer = document.querySelector('.footer-container'); 
+  FooterContainer.innerHTML = `
+  <nav class="nav-container">
+          <a class="container-buttons" href="index.html">
+            <button type="button" class="buttons">
+              <img class="icon" src="/icons/home.png" alt="Home icon" />
+            </button>
+          </a>
+          <a class="container-buttons" href="products.html">
+            <button type="button" class="buttons">
+              <img class="icon" src="/icons/food.png" alt="Home icon" />
+            </button>
+          </a>
+          <a class="container-buttons" href="order.html">
+            <button type="button" class="buttons">
+              <img class="icon" src="/icons/cart.png" alt="Home icon" />
+            </button>
+          </a>
+          <a class="container-buttons" href="checkout.html">
+            <button type="button" class="buttons">
+              <img class="icon" src="/icons/pay.png" alt="Home icon" />
+            </button>
+          </a>
+        </nav>`;
+  
+}
+createHeader();
+createFooter();
