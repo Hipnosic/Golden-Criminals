511601d504e1dc6c44243bb752598767351b1203
ImaDuhf <timperson234@gmail.com>
2022-12-20 12:56:36 +0100

    Added custom drink order function to product page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const productContainer = document.querySelector('.products');
-const db = [database.bbqs, database["best-foods"], database.breads, database.burgers, database.chocolates, database.desserts, database.drinks, database["fried-chicken"], database["ice-cream"], database["our-foods"], database.pizzas, database.porks, database.sandwiches, database.sausages, database.steaks];
-const pickedProducts = [database.bbqs[15], database["best-foods"][3], database.burgers[22], database.burgers[43]]
+const db = [database.bbqs, database['best-foods'], database.breads, database.burgers, database.chocolates, database.desserts, database.drinks, database['fried-chicken'], database['ice-cream'], database['our-foods'], database.pizzas, database.porks, database.sandwiches, database.sausages, database.steaks];
+const pickedProducts = [database.bbqs[15], database['best-foods'][3], database.burgers[22], database.burgers[43]];
 
 function products() {
     if (window.location.pathname == '/products.html') {
@@ -55,16 +55,54 @@ function addListener() {
     }
 }
 
+function customizeDrink() {
+    if (window.location.pathname == '/products.html') {
+        const products = document.querySelectorAll('.product');
+        const filters = document.querySelector('.filters');
+        const search = document.querySelector('.search-container');
+        const customOrderButton = document.querySelector('.custom-order');
+        customOrderButton.addEventListener('click', () => {
+            for (const product of products) {
+                product.remove();
+            }
+            filters.remove();
+            search.remove();
+            let customOrderForm = document.createElement('form');
+            let input = document.createElement('input');
+            let button = document.createElement('button');
+            customOrderForm.classList.add('custom-order-form');
+            input.classList.add('custom-order-input');
+            button.classList.add('custom-order-button');
+            button.innerText = 'Order';
+            button.addEventListener('click', () => {
+                let input = document.querySelector('.custom-order-input');
+                if (input.value.trim() != '') {
+                    redirect();
+                }
+            });
+
+            customOrderForm.append(input, button);
+            productContainer.append(customOrderForm);
+        });
+    }
+}
+
+function redirect() {
+    window.location.href = '/order.html';
+}
 
 function searchFunction() {
     const products = document.querySelectorAll('.product');
     // gets the input
-    input = document.getElementById('searchbar').value
+    input = document.getElementById('searchbar').value;
     let productPos = 0;
     for (const product of pickedProducts) {
-      //toUpperCase for non caps specific
-      //if the product description includes input or if the product name includes input
-        if (product.dsc.toUpperCase().includes(input.toUpperCase()) || product.name.toUpperCase().includes(input.toUpperCase())) {
+        //toUpperCase for non caps specific
+        //if the product description includes input or if the product name includes input
+        if (
+            product.dsc.toUpperCase().includes(input.toUpperCase()) ||
+            product.name.toUpperCase().includes(input.toUpperCase())
+        ) {
             /*
             for testing the search function
             console.log(productPos)
@@ -76,8 +114,7 @@ function searchFunction() {
             console.log(!drink.name.toUpperCase().includes(input.toUpperCase()));
             */
             products[productPos].style.display = '';
-        }
-        else {
+        } else {
             products[productPos].style.display = 'none';
         }
         //makes sure that the right product gets changed
@@ -100,7 +137,7 @@ function CheckAndCreateIfNoItem(productPos) {
 }
 
 function createHeader() {
-  //makes the html for header
+    //makes the html for header
     let headerContainer = document.querySelector('.header-container');
     headerContainer.innerHTML = `
     <img
@@ -118,7 +155,7 @@ function createHeader() {
     </a>`;
 }
 function createFooter() {
-  //makes the html for footer
+    //makes the html for footer
     let FooterContainer = document.querySelector('.footer-container');
     FooterContainer.innerHTML = `
     <nav class="nav-container">
@@ -147,5 +184,6 @@ function createFooter() {
 
 products();
 addListener();
+customizeDrink();
 createHeader();
 createFooter();
